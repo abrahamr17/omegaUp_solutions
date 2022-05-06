16f5f66ab4274bc57edc430ff9fe2165bbe7feb5
Abraham Romo <105086514+abrahamr17@users.noreply.github.com>
2022-05-06 18:36:29 -0500

    Create par_o_Impar.cpp

---
+++ par_o_Impar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int numero;
+	
+	cin>>numero;
+	
+	if((numero%2)==0){
+		cout<<"par"<<endl;
+	}
+	else{
+		cout<<"impar"<<endl;
+	}
+	return 0;
+}
