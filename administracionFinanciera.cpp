33d75780d7d6ce5ecaf23a90d117fb908cab97a0
Abraham Romo <105086514+abrahamr17@users.noreply.github.com>
2022-05-06 18:35:24 -0500

    Create administracionFinanciera.cpp

---
+++ administracionFinanciera.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	double a,b,c,d,e,f,g,h,i,j,k,l;
+	
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d;
+	cin>>e;
+	cin>>f;
+	cin>>g;
+	cin>>h;
+	cin>>i;
+	cin>>j;
+	cin>>k;
+	cin>>l;
+	
+	cout<<"$"<<fixed<<setprecision(2)<<(a+b+c+d+e+f+g+h+i+j+k+l)/12;
+	
+}
